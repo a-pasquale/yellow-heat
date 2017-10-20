c19017f29e72be4c91b37c96e45b172d0a37d597
Andrew Pasquale <andrew@elytra.net>
2017-10-20 16:11:39 -0400

    Write to firebase using the device id

--- fs/init.js
+++ fs/init.js
@@ -1,22 +1,25 @@
 load('api_config.js');
-load("api_http.js");
+load('api_http.js');
 load('api_gpio.js');
-load("api_timer.js");
+load('api_timer.js');
+load('api_math.js');
 
 let button = Cfg.get('app.button');
 let uid = Cfg.get('app.uid');
-let heater = Cfg.get('app.heater');
+let heater = Cfg.get('device.id');
 
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
     print("Button pushed");
     HTTP.query({
         url: 'https://yellow-heat.firebaseio.com/data/' + heater + '.json',
-        headers: { },
+        headers: { 
+            //'X-HTTP-Method-Override': 'PUT'
+        },
         data: {
-	    fuel: 1.0,
-	    message: "on",
-	    timestamp: Timer.now()
-	},
+            fuel: Math.random(),
+            message: "on",
+            timestamp: Timer.now()
+        },
         success: function(body, full_http_msg) { print(body); },
         error: function(err) { print(err); },
 });
