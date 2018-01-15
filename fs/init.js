3c3ee293cf87d50b3686b4aa2f0dd3ecd6010785
Andrew Pasquale <andrew@elytra.net>
2018-01-15 11:33:17 -0500

    Read values from fuel level sender when triggered by burner call.

--- fs/init.js
+++ fs/init.js
@@ -3,24 +3,61 @@ load('api_http.js');
 load('api_gpio.js');
 load('api_timer.js');
 load('api_math.js');
+load('api_adc.js');
+load("api_rpc.js");
 
 let button = Cfg.get('app.button');
 let uid = Cfg.get('app.uid');
 let heater = Cfg.get('device.id');
 
+let fuel = 33;      // pin for fuel guage
+let full = 3935.0;  // value at full
+let empty = 40.0;   // value at empty
+
+let burn = 18;      // pin for burner
+let burning = 0;    // burner status
+
+ADC.enable(fuel);
+
+RPC.addHandler('Fuel', function() {
+  return ADC.read(33) / full;
+});
+
+
+GPIO.set_mode(burn, GPIO.MODE_INPUT);
+
+GPIO.set_int_handler(burn, GPIO.INT_EDGE_NEG, function(burn) {
+  let calling = GPIO.read(burn);
+  if (burning && !calling) {
+    burning = 0;
+    print('Turning off');
+    postFuelLevel();
+  } else if (!burning && calling) {
+    burning = 1;
+    print('Turning on');
+    postFuelLevel();
+  }
+  print('Pin', burn, 'got interrupt');
+}, null);
+GPIO.enable_int(burn);
+
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
     print("Button pushed");
-    HTTP.query({
+    postFuelLevel();
+}, null);
+
+function postFuelLevel() {
+  HTTP.query({
         url: 'https://yellow-heat.firebaseio.com/data/' + heater + '.json',
         headers: { 
             //'X-HTTP-Method-Override': 'PUT'
         },
         data: {
-            fuel: Math.random(),
-            message: "on",
+            fuel: ADC.read(fuel) / full,
+            message: burning ? "on" : "off",
             timestamp: Timer.now()
         },
         success: function(body, full_http_msg) { print(body); },
         error: function(err) { print(err); },
-});
-}, null);
+  });
+}
