08ff0a7f238dc09160cba592e93d631564d06106
Andrew Pasquale <andrew@elytra.net>
2018-10-17 12:48:59 -0400

    Add DS18B20 temperature sensor.

--- fs/init.js
+++ fs/init.js
@@ -5,6 +5,9 @@ load('api_timer.js');
 load('api_math.js');
 load('api_adc.js');
 load("api_rpc.js");
+load('api_arduino_onewire.js');
+load('ds18b20.js');
+
 
 let button = Cfg.get('app.button');
 let uid = Cfg.get('app.uid');
@@ -16,6 +19,12 @@ let empty = 40.0;   // value at empty
 
 let burn = 18;      // pin for burner
 let burning = 0;    // burner status
+
+// Initialize OneWire library
+let ow = OneWire.create(21 /* pin */);
+let n = 0;              // Number of sensors found on the OneWire bus
+let rom = ['01234567']; // Sensors addresses
+let tempF;
 
 ADC.enable(fuel);
 
@@ -65,3 +74,36 @@ function postFuelLevel() {
         error: function(err) { print(err); },
   });
 }
+
+// This function prints temperature every minute
+Timer.set(60*1000 /* 1 min */, Timer.REPEAT, function() {
+
+  if ((n = searchSens()) === 0) {
+    print('No device found');
+  }
+
+  for (let i = 0; i < n; i++) {
+    let t = getTempF(ow, rom[i]);
+    if (isNaN(t)) {
+      print('No device found');
+      break;
+    } else {
+      print('Sensor#', i, 'Temperature:', t, '*F');
+      tempF = t;
+      HTTP.query({
+        url: 'https://yellow-heat.firebaseio.com/temp/' + uid + "/" + heater + '.json',
+        data: {
+            sensor: i,
+            temp: tempF,
+            timestamp: Timer.now()
+        },
+        success: function(body, full_http_msg) { print(body); },
+        error: function(err) { print(err); },
+  });
+    }
+  }
+}, null);
+
+RPC.addHandler('TempF', function() {
+  return tempF;
+});
