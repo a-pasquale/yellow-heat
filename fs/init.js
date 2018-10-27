c2a57882073739a1364042b999069fc5a3f06ba4
Andrew Pasquale <andrew@elytra.net>
2018-10-27 17:18:42 -0400

    Use Firebase auth

--- fs/init.js
+++ fs/init.js
@@ -7,9 +7,10 @@ load('api_adc.js');
 load("api_rpc.js");
 
 let button = Cfg.get('app.button');
-let uid = Cfg.get('app.uid');
 let heater = Cfg.get('device.id');
-let token = Cfg.get('app.token1') + Cfg.get('app.token2');
+let apiKey = Cfg.get('app.apiKey');
+let uid = Cfg.get('user.uid');
+let refresh_token = Cfg.get('user.refreshToken');
 
 let fuel = 33;      // pin for fuel guage
 let full = 3935.0;  // value at full
@@ -18,8 +19,10 @@ let empty = 40.0;   // value at empty
 let burn = 18;      // pin for burner
 let burning = 0;    // burner status
 
-ADC.enable(fuel);
+let f = 0;          // Current fuel level
+let t = 0;          // Current time
 
+ADC.enable(fuel);
 RPC.addHandler('readFuelLevel', function() {
   return ADC.read(33) / full;
 });
@@ -51,26 +54,40 @@ GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function()
 }, null);
 
 function postFuelLevel() {
-  let f = ADC.read(fuel) / full;
-  print("fuel", f);
-  let t = Timer.now();
+  f = ADC.read(fuel) / full;
+  print("fuel:", f);
+  t = Timer.now();
   print("Time:", t);
   
+  // Get a new auth token from Firebase,
+  // Then post to the database.
   HTTP.query({
-    url: 'https://yellow-heat.firebaseio.com/' + uid + "/" + heater + '/data/.json?auth=' + token,
-    headers: { 
-        //'X-HTTP-Method-Override': 'PUT'
-    },
+    url: 'https://securetoken.googleapis.com/v1/token?key=' + apiKey,
     data: {
-        fuel: f,
-        message: burning ? "on" : "off",
-        timestamp: t
+      'grant_type':	'refresh_token',
+      'refresh_token': refresh_token
     },
-    success: function(body, full_http_msg) {
-      print("Posted successfully to Firebase: ", body); 
+    success: function(resp) {
+      resp = JSON.parse(resp);
+      refresh_token = resp.refresh_token;
+      HTTP.query({
+        url: 'https://yellow-heat.firebaseio.com/' + uid + "/" + heater + '/data.json?auth=' + resp.id_token,
+        data: {
+          fuel: f,
+          message: burning ? "on" : "off",
+          timestamp: t
+        },
+        success: function(body, full_http_msg) {
+          print("Posted successfully to Firebase: ", body); 
+        },
+        error: function(err) { 
+          print("An error occurred when posting to Firebase: ", err); 
+        },
+      });    
     },
-    error: function(err) { 
-      print("An error occurred when posting to Firebase: ", err); 
-    },
-  });
+    error: function(err) {
+        print("Error getting new Firebase token", err)
+    }
+  })
+  
 }
