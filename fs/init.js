02d72ffb8f83110c6195baec0bc8c94a397e7d4e
Andrew Pasquale <andrew@elytra.net>
2018-10-29 10:25:29 -0400

    Use Firebase auth token. Blink LED when unable to post data.

--- fs/init.js
+++ fs/init.js
@@ -8,12 +8,14 @@ load("api_rpc.js");
 load('api_arduino_onewire.js');
 load('ds18b20.js');
 
-
-let button = Cfg.get('app.button');
-let heater = Cfg.get('device.id');
+let ledPin = 19;
+let btnPin = Cfg.get('app.button');
 let apiKey = Cfg.get('app.apiKey');
-let uid = Cfg.get('user.uid');
-let refresh_token = Cfg.get('user.refreshToken');
+let url = 'https://yellow-heat.firebaseio.com/' + Cfg.get('user.uid') + '/' + Cfg.get('device.id');
+let refresh_token = Cfg.get('user.refreshToken'); // Firebase refresh token
+let token = '';          // Firebase id token
+let refresh_time = 30; // minutes 
+let temp_time = 5;     // minutes
 
 let fuel = 33;      // pin for fuel guage
 let full = 3935.0;  // value at full
@@ -26,10 +28,10 @@ let burning = 0;    // burner status
 let ow = OneWire.create(21 /* pin */);
 let n = 0;              // Number of sensors found on the OneWire bus
 let rom = ['01234567']; // Sensors addresses
-let tempF;
+let tempF = 0;
 
-let f = 0;          // Current fuel level
-let t = 0;          // Current time
+let f = 0;              // Current fuel level
+let t = 0;              // Current time
 
 ADC.enable(fuel);
 RPC.addHandler('readFuelLevel', function() {
@@ -41,6 +43,8 @@ RPC.addHandler('postToFirebase', function() {
 }) 
 
 GPIO.set_mode(burn, GPIO.MODE_INPUT);
+GPIO.set_mode(ledPin, GPIO.MODE_OUTPUT); // LED warning if unable to post data
+GPIO.write(ledPin, 1);
 
 GPIO.set_int_handler(burn, GPIO.INT_EDGE_NEG, function(burn) {
   let calling = GPIO.read(burn);
@@ -57,57 +61,13 @@ GPIO.set_int_handler(burn, GPIO.INT_EDGE_NEG, function(burn) {
 }, null);
 GPIO.enable_int(burn);
 
-GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
+GPIO.set_button_handler(btnPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
     print("Button pushed");
     postFuelLevel();
 }, null);
 
-function getToken () {
-  HTTP.query({
-    url: 'https://securetoken.googleapis.com/v1/token?key=' + apiKey,
-    data: {
-      'grant_type':	'refresh_token',
-      'refresh_token': refresh_token
-    },
-    success: function(resp) { return resp },
-    error: function(err) {
-      print("Error getting new Firebase token", err);
-      return undefined;
-    }
-  });
-}
-
-function postFuelLevel() {
-  f = ADC.read(fuel) / full;
-  print("fuel:", f);
-  t = Timer.now();
-  print("Time:", t);
-  
-  // Get a new auth token from Firebase,
-  // Then post to the database.
-  getToken()
-  .then( function(resp) {
-    if (resp) {
-      resp = JSON.parse(resp);
-      refresh_token = resp.refresh_token;
-      HTTP.query({
-        url: 'https://yellow-heat.firebaseio.com/' + uid + "/" + heater + '/data.json?auth=' + resp.id_token,
-        data: {
-          fuel: f,
-          message: burning ? "on" : "off",
-          timestamp: t
-        },
-        success: function(body, full_http_msg) {
-          print("Posted successfully to Firebase: ", body); 
-        },
-        error: function(err) { 
-          print("An error occurred when posting to Firebase: ", err); 
-        },
-      });    
-    }
-  })
-}
-/*
+// Get a Firebase token
+function getTokens() {
   HTTP.query({
     url: 'https://securetoken.googleapis.com/v1/token?key=' + apiKey,
     data: {
@@ -116,31 +76,53 @@ function postFuelLevel() {
     },
     success: function(resp) {
       resp = JSON.parse(resp);
+      token = resp.id_token;
       refresh_token = resp.refresh_token;
-      HTTP.query({
-        url: 'https://yellow-heat.firebaseio.com/' + uid + "/" + heater + '/data.json?auth=' + resp.id_token,
-        data: {
-          fuel: f,
-          message: burning ? "on" : "off",
-          timestamp: t
-        },
-        success: function(body, full_http_msg) {
-          print("Posted successfully to Firebase: ", body); 
-        },
-        error: function(err) { 
-          print("An error occurred when posting to Firebase: ", err); 
-        },
-      });    
+      print("tokens refreshed");
+      print(resp.expires_in);
+      print("expires in seconds");
+      GPIO.write(ledPin, 1); // Turn on LED on successful post.
     },
-
-  })
-  
+    error: function(err) {
+      print("error refreshing tokens");
+      print(JSON.stringify(err));
+      GPIO.blink(ledPin, 1000, 1000); // Blink warning LED.
+    }
+  });
 }
-*/
+getTokens();
 
-// This function prints temperature every minute
-Timer.set(60*1000 /* 1 min */, Timer.REPEAT, function() {
+// Refresh the tokens
+Timer.set(refresh_time*60*1000 /* min x sec x ms */, Timer.REPEAT, function() {
+  getTokens();
+}, null);
 
+function postFuelLevel() {
+  f = ADC.read(fuel) / full;
+  print("fuel:", f);
+  t = Timer.now();
+  print("Time:", t);
+  
+  HTTP.query({
+    url: url + '/data.json?auth=' + token,
+    data: {
+      fuel: f,
+      message: burning ? "on" : "off",
+      timestamp: t
+    },
+    success: function(body, full_http_msg) {
+      print("Posted successfully to Firebase: ", body);
+      GPIO.write(ledPin, 1); // Turn on LED on successful post.
+    },
+    error: function(err) { 
+      print("An error occurred when posting to Firebase: ", err);
+      GPIO.blink(ledPin, 1000, 1000); // Blink warning LED.
+    }
+  });    
+};
+
+// Get temperature every minute
+Timer.set(temp_time*60*1000 /* min x sec x ms */, Timer.REPEAT, function() {
   if ((n = searchSens()) === 0) {
     print('No device found');
   }
@@ -153,25 +135,25 @@ Timer.set(60*1000 /* 1 min */, Timer.REPEAT, function() {
     } else {
       print('Sensor#', i, 'Temperature:', t, '*F');
       tempF = t;
-      getToken()
-      .then( function(resp) {
-        if (resp) {
-          HTTP.query({
-            url: 'https://yellow-heat.firebaseio.com/temp/' + uid + "/" + heater + '.json?auth=' + resp.id_token,
-            data: {
-              sensor: i,
-              temp: tempF,
-              timestamp: Timer.now()
-            },
-            success: function(body, full_http_msg) { print(body); },
-            error: function(err) { print(err); },
-          });
-        }
-      })
     }
   }
+  HTTP.query({
+    url: url + '/temp.json?auth=' + token,
+    data: {
+      temp: tempF,
+      timestamp: Timer.now()
+    },
+    success: function(body, full_http_msg) { 
+      print("Temp posted to Firebase");
+      GPIO.write(ledPin, 1); // Turn off warning LED on successful post. 
+    },
+    error: function(err) { 
+      print(err); 
+      GPIO.blink(ledPin, 1000, 1000); // Blink warning LED.
+    },
+  });
 }, null);
 
 RPC.addHandler('TempF', function() {
-return tempF;
+  return tempF;
 });
